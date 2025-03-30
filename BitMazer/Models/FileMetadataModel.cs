2559885b5d994536c8e8b7bf6dd2e8b1ab797c49
Teofan Gradinariu <teofang23@gmail.com>
2025-03-30 02:42:26 +0200

    ui improvements

--- BitMazer/Models/FileMetadataModel.cs
+++ BitMazer/Models/FileMetadataModel.cs
@@ -1,17 +1,22 @@
 ﻿using Microsoft.Extensions.FileSystemGlobbing;
 using System.Text;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace BitMazer.Models
 {
     public class FileMetadataModel(string fileName, string fileType, float fileSize, string encAlg)
     {
+        [JsonPropertyName("fileName")]
         public string FileName { get; set; } = fileName;
 
+        [JsonPropertyName("fileType")]
         public string FileType { get; set; } = fileType;
 
+        [JsonPropertyName("fileSize")]
         public float FileSize { get; set; } = (float)Math.Round((fileSize / 1024.0 / 1024.0), 2);
 
+        [JsonPropertyName("encryptionAlgorithm")]
         public string EncAlg { get; set; } = encAlg;
     }
 }
